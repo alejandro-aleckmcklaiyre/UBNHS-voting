54091c907df27c78b026ed77a361e0a74d8c3064
alejandro-aleckmcklaiyre <aleck.alejandro04@gmail.com>
2025-08-04 05:02:31 +0800

    modify login and voters page

--- php/auth/login.php
+++ php/auth/login.php
@@ -3,24 +3,30 @@ session_start();
 require_once '../config/db_config.php';
 require_once '../config/session_config.php';
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username'] ?? '');
     $password = trim($_POST['password'] ?? '');
 
+
     if ($username === '' || $password === '') {
-        header('Location: /ubnhs-voting/pages/admin_login/admin_login.html?error=empty');
+        header('Location: ../../index.php?page=admin_login&error=empty');
         exit();
     }
 
+
     // Use session_config's loginAdmin function
     if (loginAdmin($username, $password)) {
-        header('Location: /ubnhs-voting/pages/admin_dashboard/admin_dashboard.html');
+        header('Location: ../../index.php?page=admin_dashboard');
         exit();
     } else {
-        header('Location: /ubnhs-voting/pages/admin_login/admin_login.html?error=invalid');
+        header('Location: ../../index.php?page=admin_login&error=invalid');
         exit();
     }
 } else {
-    header('Location: /ubnhs-voting/pages/admin_login/admin_login.html');
+    header('Location: ../../index.php?page=admin_login');
     exit();
 }
+
+
+
